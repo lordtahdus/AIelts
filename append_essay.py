04fc987f9c0e23eedf604470ac7e0d793cf6ac01
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-05-16 14:29:53 +1000

    added 6 more samples

--- append_essay.py
+++ append_essay.py
@@ -1,19 +1,20 @@
 
-index = 39
+index = 45
 
-link = ""
+link = "https://writing9.com/text/5ec80ab475f0910018dec589"
 
 topic = """\
-Forests produce fresh oxygen and participate in regulating climate. But every year tree cover of our planet is lessening due to deforestation.
-What are the primary causes of deforestation? 
-To what results may it lead?\
+It is thought by some that it is better to live in a city while others believe that life is better in the countryside. Discuss both side and give your opinion.\
 """
 
 essay = """\
-As deforestation frequently occurs in many regions around the globe, it poses terrible threats to human survival. This essay will first prove deforestation resulting from human beings and climate change and later admit hotter climate and soil erosion as possible consequences.
-To begin with, the primary reason for deforestation is human activities to fulfill the demands. It is a universal consensus that in order to solve the shortage of accommodation, many regions are cutting multitude numbers of trees for land to construct or for city expansion. Also, large-scale deforestation for woods is regarded as one of the most harmful activities people have done to the environment. In addition, it is hard to deny that wildfire due to climate change can reduce a large proportion of trees on Earth. Recently, a drought last for 2 years caused a tremendous bushfire season in Australia between 2019 and 2020, which makes governors declare a state of emergency.
-On the other hand, the negative effects of deforestation will be a hotter climate and soil erosion. First of all, jungles play an essential role in regulating the air. Tree leaves convert carbon dioxide into oxygen, which means if trees get chopped down, the CO2 released by factories and cars will increase in amount. Therefore, the overall temperature can rise due to deforestation. Secondly, it is proven that when trees do not cover the land, the soil will be eroded rapidly. Conspicuously, poor soil, which plants hardly grow on, may become a wasteland in no time.
-In conclusion, the reason why trees cover the planet is lessening is people deforest for land and for wood and long-term drought. Such events will lead to global warming and soil wearing away, which can bring many species to the verge of extinction.\
+Many people have many perspectives about where to live. Although some might say that living in urban is more convenient, others think that there are too many chaotic and pollution. This essay will explore advantages and disadvantages of both places and give an opinion at the end.
+
+Nowadays, lots of major cities are facing with congested traffic because most of residents are dependent on their private car and commute to work where is their workplace located. Therefore, there are a lot of air pollution in the heart of the town and not only this but also too much noisy, especially during rush hour. However, somebody choose to rent or buy a condominium which is located in the right center of cities as this place is nearby their company and there are plenty of the convenient shopping malls and entertainment places, such as a cinema, a restaurant, a fitness center and so on.
+
+In contrast, some families decide to live on the outskirts as it is more quiet and more fresh air, so they can have a better quality of living than stay inside the city which is fully of crowds. On the other hand, these groups may have to battle through the traffic problems as they need to travel to work.
+
+To sum up, there are pros and cons between living in an urban and countryside. People have many options to decide whether which place is suite them. In my view, I think living in the city is the best option as we can go to work easily.\
 """
 
 
