814ce2422b2f32026942525c6cf39e4bdb14a45a
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-05-16 15:02:17 +1000

    added 1 more

--- append_essay.py
+++ append_essay.py
@@ -1,20 +1,21 @@
 
-index = 45
+index = 46
 
-link = "https://writing9.com/text/5ec80ab475f0910018dec589"
+link = "https://writing9.com/text/5eafca6273403800189c0e6e"
 
 topic = """\
-It is thought by some that it is better to live in a city while others believe that life is better in the countryside. Discuss both side and give your opinion.\
+Some educationalists think that a programme of international exchange visits will offer various benefits for teenage school students.
+Do you think the advantages of it outweigh the disadvantages?\
 """
 
 essay = """\
-Many people have many perspectives about where to live. Although some might say that living in urban is more convenient, others think that there are too many chaotic and pollution. This essay will explore advantages and disadvantages of both places and give an opinion at the end.
+In our modern life, one of the most important aspect is education.Respectively, programme of international exchange became a popular in resent year. I am on the view that advantages of it overweight disadvantages. In this essay I will explore my ideas with reasons and examples before coming to a conclusion.
 
-Nowadays, lots of major cities are facing with congested traffic because most of residents are dependent on their private car and commute to work where is their workplace located. Therefore, there are a lot of air pollution in the heart of the town and not only this but also too much noisy, especially during rush hour. However, somebody choose to rent or buy a condominium which is located in the right center of cities as this place is nearby their company and there are plenty of the convenient shopping malls and entertainment places, such as a cinema, a restaurant, a fitness center and so on.
+On the one hand, there some drawbacks of this programme of international exchange, especially among teenage school students. Firstly, it chance to get acquainted with new culture. In other words, this programme way to adopt different cultures and lifestyles which are not suitable for our value. Secondly, we can travel and enjoy around cities with spectacular view for free and it really helps to leave our comfort zone.
 
-In contrast, some families decide to live on the outskirts as it is more quiet and more fresh air, so they can have a better quality of living than stay inside the city which is fully of crowds. On the other hand, these groups may have to battle through the traffic problems as they need to travel to work.
+On the other hand, the mean benefit of studying abroad is improve knowledge level. I am sure that, exchange opinions with teachers and professors from different countries, absolutely help to develop our knowledge to new degree. For instance, we can learn something new, expand our outlook, recognise about their culture, traditions and history from them and teach others what we know. In addition, another important advatage is that students can make new friends and build relationships. Friends always help to relax and adapt. 
 
-To sum up, there are pros and cons between living in an urban and countryside. People have many options to decide whether which place is suite them. In my view, I think living in the city is the best option as we can go to work easily.\
+To sum up, when we analyse the issue in depth, it becomes crystal clear that programme of international exchange  has more advantages than disadvantages also it really help to students braking knowledge and start new life.\
 """
 
 
