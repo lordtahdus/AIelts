afa20edb3cc2909d63e33da034f856dc38d8bf39
Vincent Su <121150865+lordtahdus@users.noreply.github.com>
2023-05-20 01:26:04 +1000

    minor changes to regenerate.py and edit spacing in essay 38

--- regenerate.py
+++ regenerate.py
@@ -14,25 +14,9 @@ messages.append({"role": "system", "content": system_msg})
 index = 40
 
 
-syntaxes = [
-    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease edit the essay according to IELTS structure. Also, estimate the score.',
-    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease provide me detailed feedback in Vietnamese with clear explanations, based on four scoring criteria:\nTask Response\nCoherence and Cohesion\nLexical Resource\nGrammatical Range and Accuracy',
-    "Đánh giá Task Response trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
-    "Đánh giá Coherence and Cohesion trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
-    "Đánh giá Lexical Resource trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
-    "Đánh giá Grammatical Range and Accuracy trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
-    "Estimate carefully the score of each criteria"
-]
-headings = [
-    "Feedback:\n\nTask Response:\n",
-    "Coherence and Cohesion:\n",
-    "Lexical Resource:\n",
-    "Grammatical Range and Accuracy:\n",
-    "Score:\n"
-]
 
-directory = 'processed_essay'
-file = f'{directory}\essay_{index}.txt' 
+
+file = f'processed_essay/essay_{index}.txt' 
 
 with open(file, 'r', encoding="utf-8") as f:
     lines = f.readlines()
@@ -49,7 +33,11 @@ with open(file, 'r', encoding="utf-8") as f:
     ]
 
     topic = lines[1:sep_indexes[0]]
+    topic = ''.join(topic).strip()
+
     essay = lines[sep_indexes[0] + 1: sep_indexes[1]]
+    essay = ''.join(essay).strip()
+
     revised = lines[sep_indexes[1] + 1: sep_indexes[2]]
     tr = lines[sep_indexes[2] + 1: sep_indexes[3]]
     cc = lines[sep_indexes[3] + 1: sep_indexes[4]]
@@ -57,36 +45,76 @@ with open(file, 'r', encoding="utf-8") as f:
     ga = lines[sep_indexes[5] + 1: sep_indexes[6]]
     score = lines[sep_indexes[6] + 1:]
 
+    contents = [
+        ''.join(revised).strip(),
+        '',
+        ''.join(tr).strip(),
+        ''.join(cc).strip(),
+        ''.join(lr).strip(),
+        ''.join(ga).strip(),
+        ''.join(score).strip(),
+    ]
+
     # print(tr)
     # print('\n\n')
-    print(''.join(tr).strip())
+    # print(''.join(tr).strip())
     # print(''.join(cc))
     # print(''.join(lr))
     # print(''.join(ga))
     # print(''.join(score))
-    
 
+
+syntaxes = [
+    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease edit the essay according to IELTS structure. Also, estimate the score.',
+    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease provide me detailed feedback in Vietnamese with clear explanations, based on four scoring criteria:\nTask Response\nCoherence and Cohesion\nLexical Resource\nGrammatical Range and Accuracy',
+    "Đánh giá Task Response trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Coherence and Cohesion trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Lexical Resource trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Grammatical Range and Accuracy trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Estimate carefully the score of each criteria"
+]
+headings = [
+    "Revised:\n",
+    "",
+    "Feedback:\n\nTask Response:\n",
+    "Coherence and Cohesion:\n",
+    "Lexical Resource:\n",
+    "Grammatical Range and Accuracy:\n",
+    "Score:\n"
+]
 
 
 def run():
-    # while input != "quit()":
-    with open(f"essay_{index}.txt", "a", encoding="utf-8") as f:
+    # move the old_essay to old_essay folder
+    os.rename(f'processed_essay/essay_{index}.txt', f'old_essay/essay_{index}.txt')
+
+    user_options = [0,0,0,0,0,0,0]
+    print('input 0 or 1')
+    user_options[0] = int(input('Regenerate Revised? '))
+    user_options[2] = int(input('Regenerate Task Response? '))
+    user_options[3] = int(input('Regenerate Coherence and Cohesion? '))
+    user_options[4] = int(input('Regenerate Lexical Resource? '))
+    user_options[5] = int(input('Regenerate Grammatical Range and Accuracy? '))
+    user_options[6] = int(input('Regenerate Score? '))
+
+    with open(f"processed_essay/essay_{index}.txt", "w", encoding="utf-8") as f:
+        f.write(f"""Topic:\n\n{topic}\n\nEssay:\n\n{essay}\n\n""")
+
         for i in range(0,7):
             message = syntaxes[i]
             # Revised
-            if i == 0:
+            if i == 0 and user_options[i] == 1:
                 messages.append({"role": "user", "content": message})
-                print("Revised:\n", file = f)
             # General feedback
             elif i == 1:
-                messages.pop(1)
+                if len(messages) == 2:
+                    messages.pop(1)
                 messages.append({"role": "user", "content": message})
             # Specific Criteria and Score
-            else:
+            elif user_options[i] == 1:
                 if len(messages) == 4:
                     messages.pop(3)
                 messages.append({"role": "user", "content": message})
-                print(headings[i-2], file = f)
 
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -97,4 +125,13 @@ def run():
             if i == 1:
                 messages.append({"role": "assistant", "content": reply})
             else:
-                print(reply + '\n\n', file = f)
+                f.write(headings[i])
+                if user_options[i] == 0:
+                    f.write('\n' + contents[i] + '\n\n')
+                else:
+                    print(reply + '\n\n', file = f)
+
+
+
+if __name__ == "__main__":
+    run()
