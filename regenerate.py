cc84496ae30962a3d5af07e438151cb035704827
Vincent Su <121150865+lordtahdus@users.noreply.github.com>
2023-05-19 23:50:26 +1000

    code program for re-generate (unfinished)

--- regenerate.py
+++ regenerate.py
@@ -1,4 +1,6 @@
 import openai
+import os
+
 from decouple import config
 
 openai.api_key = config('OPENAI_KEY_1')
@@ -9,8 +11,25 @@ messages.append({"role": "system", "content": system_msg})
 
 
 
-index = 39
+index = 40
 
+
+syntaxes = [
+    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease edit the essay according to IELTS structure. Also, estimate the score.',
+    f'This is IELTS writing task 2.\n\nTopic:\n"{topic}"\n\nEssay:\n"{essay}"\nPlease provide me detailed feedback in Vietnamese with clear explanations, based on four scoring criteria:\nTask Response\nCoherence and Cohesion\nLexical Resource\nGrammatical Range and Accuracy',
+    "Đánh giá Task Response trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Coherence and Cohesion trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Lexical Resource trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Đánh giá Grammatical Range and Accuracy trong bài viết của tôi một cách chi tiết hơn và nêu ra lỗi sai (nếu có)",
+    "Estimate carefully the score of each criteria"
+]
+headings = [
+    "Feedback:\n\nTask Response:\n",
+    "Coherence and Cohesion:\n",
+    "Lexical Resource:\n",
+    "Grammatical Range and Accuracy:\n",
+    "Score:\n"
+]
 
 directory = 'processed_essay'
 file = f'{directory}\essay_{index}.txt' 
@@ -38,12 +57,44 @@ with open(file, 'r', encoding="utf-8") as f:
     ga = lines[sep_indexes[5] + 1: sep_indexes[6]]
     score = lines[sep_indexes[6] + 1:]
 
-    print(topic)
-    print(essay)
+    # print(tr)
+    # print('\n\n')
+    print(''.join(tr).strip())
+    # print(''.join(cc))
+    # print(''.join(lr))
+    # print(''.join(ga))
+    # print(''.join(score))
+    
 
-    syntax = []
-    syntax 
 
-    # i = 0
-    # while i <= 7:
-    #     message
+
+def run():
+    # while input != "quit()":
+    with open(f"essay_{index}.txt", "a", encoding="utf-8") as f:
+        for i in range(0,7):
+            message = syntaxes[i]
+            # Revised
+            if i == 0:
+                messages.append({"role": "user", "content": message})
+                print("Revised:\n", file = f)
+            # General feedback
+            elif i == 1:
+                messages.pop(1)
+                messages.append({"role": "user", "content": message})
+            # Specific Criteria and Score
+            else:
+                if len(messages) == 4:
+                    messages.pop(3)
+                messages.append({"role": "user", "content": message})
+                print(headings[i-2], file = f)
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages)
+            reply = response["choices"][0]["message"]["content"]
+
+            # save general feedback in messages but not print 
+            if i == 1:
+                messages.append({"role": "assistant", "content": reply})
+            else:
+                print(reply + '\n\n', file = f)
