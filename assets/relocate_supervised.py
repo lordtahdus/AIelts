47c762eb5a7c576f7dd3a3d49ad6cc21e3fd14ed
Vincent Su <121150865+lordtahdus@users.noreply.github.com>
2023-05-29 21:19:19 +1000

    Code new tool to relocate the supervised essay to assets with corresponding band

---
+++ assets/relocate_supervised.py
@@ -0,0 +1,24 @@
+# import required module
+import os
+# assign directory
+directory = 'supervised_essay'
+ 
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    file = os.path.join(directory, filename)
+
+    # checking if it is a file
+    if os.path.isfile(file):
+        print(file[-12:])
+    
+    # read file
+    with open(file, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "Overall:" in line:
+                band = line[9:].strip()
+                print(band)
+
+    # move the supervised_essay to assets with corresponding band
+    os.rename(f'{file}', f'assets/band_{band}/{file[-12:]}')
