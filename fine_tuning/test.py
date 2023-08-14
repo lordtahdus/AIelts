b92f66a48baa7060307c4d8d26085dfc2fcc6d53
longho <longhaiho696@gmail.com>
2023-08-14 22:58:52 +1000

    small change to keys

--- fine_tuning/test.py
+++ fine_tuning/test.py
@@ -2,9 +2,9 @@ import openai
 from decouple import config
 import os
 
-openai.api_key = config('OPENAI_KEY_3')
+openai.api_key = config('OPENAI_KEY_1')
 
-FINE_TUNED_MODEL = "babbage:ft-personal-2023-08-07-05-53-27"
+FINE_TUNED_MODEL = "babbage:ft-personal-2023-08-13-14-34-48"
 
 # ada:ft-personal-2023-08-07-05-45-45
 # babbage:ft-personal-2023-08-07-05-53-27
@@ -44,42 +44,42 @@ In conclusion, I do believe that the loss of animals and plants is one of the en
 # """
 
 
-def request_ChatGPT(messages):
-    prompt = "Please rewrite this in Vietnamese: " + messages 
+# def request_ChatGPT(messages):
+#     prompt = "Please rewrite this in Vietnamese: " + messages 
 
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo",  
-        prompt=prompt,
-        max_tokens=1500, 
-    )
+#     response = openai.Completion.create(
+#         model="gpt-3.5-turbo",  
+#         prompt=prompt,
+#         max_tokens=1500, 
+#     )
     
-    reply = response["choices"][0]["message"]["content"]
+#     reply = response["choices"][0]["message"]["content"]
     
     
-    if os.path.exists("fine_tuning/last_used_index.txt"):
-        with open("last_used_index.txt", "r") as index_file:
-            last_used_index = int(index_file.read())
+#     if os.path.exists("fine_tuning/last_used_index.txt"):
+#         with open("last_used_index.txt", "r") as index_file:
+#             last_used_index = int(index_file.read())
  
-    else:
-        last_used_index = 1
+#     else:
+#         last_used_index = 1
    
-    filename = f"fine_tuning/rewritten_vietnamese{last_used_index + 1}.txt"
+#     filename = f"fine_tuning/rewritten_vietnamese{last_used_index + 1}.txt"
 
 
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write(reply)
+#     with open(filename, "w", encoding="utf-8") as file:
+#         file.write(reply)
 
-        # Update the last used index
-    last_used_index = last_used_index + 1
-
-
-    print(f"Rewritten Vietnamese version saved to '{filename}'")
+#         # Update the last used index
+#     last_used_index = last_used_index + 1
 
 
-    with open("fine_tuning/last_used_index.txt", "w") as index_file:
-        index_file.write(str(last_used_index))
+#     print(f"Rewritten Vietnamese version saved to '{filename}'")
+
+
+#     with open("fine_tuning/last_used_index.txt", "w") as index_file:
+#         index_file.write(str(last_used_index))
         
-    print("Updated index!")
+#     print("Updated index!")
     
 
 YOUR_PROMPT += "\n\n###\n\n"
@@ -91,7 +91,7 @@ response = openai.Completion.create(
     max_tokens = 1600
 )
 
-request_ChatGPT(response)
+# request_ChatGPT(response)
 
 print(response)
 
