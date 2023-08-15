1b0353532f67a1915cff0c43ee88236ae79bfc5a
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-08-15 12:13:16 +1000

    added token count

--- fine_tuning/token_cal.py
+++ fine_tuning/token_cal.py
@@ -1,7 +1,18 @@
 import tiktoken
 
 # enc = tiktoken.get_encoding("cl100k_base")
-enc = tiktoken.get_encoding("r50k_base")
+# enc = tiktoken.get_encoding("r50k_base")
 
-def token_calculate(encoding = ""):
-    #TODO
+
+
+def token_to_call(encoding, prompt, response, allow_model_token = 2046):
+    ''' Returns the number of tokens required for chatgpt call'''
+    
+    enc = tiktoken.get_encoding(encoding)
+    
+    prompt_tokens = len(enc.encode(prompt)) # or we can just use fixed value
+    response_tokens = len(enc.encode(response))
+    
+    total_tokens = allow_model_token - prompt_tokens + response_tokens
+    
+    return total_tokens
